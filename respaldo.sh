a6edb69f6a4c688c6f787b6fe78584e330869316
Oscar Espinosa <oscare_@outlook.com>
2019-03-10 11:50:05 -0500

    script para realizar el respaldo en formato tar.gz
    Se realiza el respaldo de los sitios alojados en /var/www y del directorio de drupal (en caso de haberse instalado en /html/drupal)

---
+++ respaldo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Script para realizar el respaldo del sitio
+#Listamos los sitios alojados en /var/www, quitando el dir html/
+for d in $(ls  /var/www | grep -v html); do
+	echo dir: $d
+done
+#comprimimos todos los sitios dentro de respaldo.tar.gz
+#tambien agregamos el directorio de drupal en caso de encontrarse
+#dentro de /html en lugar de /var/www/
+tar -C /var/www -czf respaldo.tar.gz $(ls /var/www | grep -v html & 
+if [ -d /var/www/html/drupal ]; then
+	#aprovechamos el cambio de directorio dado a tar con -C
+	#asi solo escribimos la ruta relativa desde /var/www
+	echo dir: html/drupal
+fi)
