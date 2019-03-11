5a49a43e1001e75e4f667d17f06e991accff4f25
jmanjma <45376226+jmanjma@users.noreply.github.com>
2019-03-10 22:35:20 -0600

    Update dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -2,7 +2,7 @@
 
 function main(){
 	`yum -y update`
-	`yum -y install php php-gd php-dom php-pdo php-mbstring php-pgsql postgresql postgresql-server postgresql-contrib`
+	`yum -y install bc php php-gd php-dom php-pdo php-mbstring php-pgsql postgresql postgresql-server postgresql-contrib`
 }
 
 main
