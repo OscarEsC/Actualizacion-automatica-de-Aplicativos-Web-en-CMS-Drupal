3b2582830c95b180ceb7ec26b6346d8756ded9b6
jmanjma <45376226+jmanjma@users.noreply.github.com>
2019-03-10 23:07:56 -0600

    Update dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 function main(){
-	`yum -y update`
-	`yum -y install bc php php-gd php-dom php-pdo php-mbstring php-pgsql postgresql postgresql-server postgresql-contrib`
+        `yum -y update 2> /dev/null`
+        echo "Sistema actualizado." | tee -a reporte.txt
+        `yum -y install php php-gd php-dom php-pdo php-mbstring php-pgsql postgresql postgresql-server postgresql-contrib 2> /dev/null`
+        echo "Dependencias instaladas correctamente." | tee -a reporte.txt
 }
 
 main
+
