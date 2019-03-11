7cebc57db7beda01161fd42b0c469e1ceee895b7
Oscar Espinosa <oscare_@outlook.com>
2019-03-10 20:54:20 -0600

    Respaldo de sitios y contenido de drupal
    Se separan los sitios y el contenido de drupal en distintos archivos tar. La compresion de drupal se hace con drush ahora

--- respaldo.sh
+++ respaldo.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #Script para realizar el respaldo del sitio
 #Listamos los sitios alojados en /var/www, quitando el dir html/
-for d in $(ls  /var/www | grep -v html); do
-	echo dir: $d
-done
-#comprimimos todos los sitios dentro de respaldo.tar.gz
-#tambien agregamos el directorio de drupal en caso de encontrarse
-#dentro de /html en lugar de /var/www/
-tar -C /var/www -czf /tmp/respaldo.tar.gz $(ls /var/www | grep -v html & 
+#comprimimos todos los sitios dentro de respaldo_sitios.tar.gz
+#eliminamos el archivo si existe
+if [ -f /tmp/respaldo_sitios.tar.gz ]; then
+	rm -f /tmp/respaldo_sitios.tar.gz
+fi
+
+#Hacemos el respaldo de los sitios en /var/www
+tar -C /var/www -czf /tmp/respaldo_sitios.tar.gz $(ls /var/www | grep -v html | grep -v drupal)
+
+#respaldo del proyecto de drupal, dependiendo de la ruta donde se encuentre
 if [ -d /var/www/html/drupal ]; then
-	#aprovechamos el cambio de directorio dado a tar con -C
-	#asi solo escribimos la ruta relativa desde /var/www
-	echo dir: html/drupal
-fi)
+	#Necesitamos cambiarnos al directorio donde esta drupal para poder usar
+	#el comando drush del proyecto
+	cd /var/www/html/drupal
+	drush archive-dump --tar-options="--exclude=%files --exclude=.git" --preserve-symlinks --overwrite --destination=/tmp/respaldo_drupal.tar.gz
+fi
+
+if [ -d /var/www/drupal ]; then
+	cd /var/www/drupal
+	drush archive-dump --tar-options="--exclude=%files --exclude=.git" --preserve-symlinks --overwrite --destination=/tmp/respaldo_drupal.tar.gz
+fi
