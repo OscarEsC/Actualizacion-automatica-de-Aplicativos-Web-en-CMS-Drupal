56c68163a7f5ce6762c19b1f2952da33efb42561
Oscar Espinosa <oscare_@outlook.com>
2019-03-11 08:46:36 -0600

    No instala postgresql
    Se han quitado los paquetes de postgresql porque se manejaran distinto a los demas en el script pgsqlDrupal.sh

--- dependencias.sh
+++ dependencias.sh
@@ -3,7 +3,7 @@
 function main(){
         sudo yum -y update 2> /dev/null
         echo "Sistema actualizado." | tee -a reporte.txt
-        sudo yum -y install php-cli php-zip wget unzip php php-gd php-dom php-pdo php-mbstring php-pgsql postgresql postgresql-server 2> /dev/null
+        sudo yum -y install php-cli php-zip wget unzip php php-gd php-dom php-pdo php-mbstring php-pgsql 2> /dev/null
         echo "Dependencias instaladas correctamente." | tee -a reporte.txt
 }
 
