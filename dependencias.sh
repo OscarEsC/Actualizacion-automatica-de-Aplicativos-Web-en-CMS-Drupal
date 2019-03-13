8a753bd977d99b31661526c29af67639fc6a946a
Oscar Espinosa <oscare_@outlook.com>
2019-03-13 13:56:48 -0600

    Deteccion independiente del core en Debian
    Ahora detecta cualquier SO Debian (8 y 9) independiente del core instalado.
    Se usa una variable de entorno para el gestor de paquetes y se reduce el funcionamiento
    principal a una sola funcion

--- dependencias.sh
+++ dependencias.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
-#EJECUTAR COMO ROOT!
 #Instalador de las dependencias necesarias para el programa
-#Dependiendo si el SO es CentOs o Debian, se ejecuta la funcion adecuada
+#Dependiendo si el SO es CentOs o Debian, se ejecuta el gestor de paquetes debido
 
-#Funcion de instalacion de dependencias en distribuciones Red Hat
-function dependencias_rh(){
-    yum -y update 2> /dev/null
+#IMPORTANTE
+#Se necesitan permisos de sudo sobre el gestor de paquetes
+
+
+#Funcion de instalacion de dependencias en distribuciones Red Hat y Debian
+function dependencias(){
+    #ejecutamos el comando asociado a la variable de entorno pm
+    sudo $pm -y update 2> /dev/null
     echo "Sistema actualizado." | tee -a reporte.txt
-    yum -y install php-cli php-zip wget unzip php php-gd php-dom php-pdo php-mbstring php-pgsql 2> /dev/null
-    echo "Dependencias instaladas correctamente." | tee -a reporte.txt
-}
-
-#Funcion de instalacion de dependencias en distribuciones Debian
-function dependencias_dev(){
-	apt -y update 2> /dev/null
-	echo "Sistema actualizado." | tee -a reporte.txt
-    apt -y install php-cli php-zip wget unzip php php-gd php-dom php-pdo php-mbstring php-pgsql 2> /dev/null
+    sudo $pm -y install php-cli php-zip wget unzip php php-gd php-dom php-pdo php-mbstring php-pgsql 2> /dev/null
     echo "Dependencias instaladas correctamente." | tee -a reporte.txt
 }
 
 #Funcion que revisa la distribucion base
-function main (){
+function revisa_distro (){
 	#Si es Debian 9
-	uname -r | grep 4.9.0-8-amd64 > /dev/null
+	uname -r | grep "4.9.0-\(.*\)-amd64" > /dev/null
 	if [ $? -eq 0 ]; then
 		echo "Debian 9 detectado" | tee -a reporte.txt
-		dependecias_dev
-	else 
+		#pm es una variable de entorno
+		pm=apt
+		dependencias
+	else
 		#Si es CentOs 7
 		uname -r | grep "3.10.0-\(.*\).el7.x86_64" > /dev/null
 		if [ $? -eq 0 ]; then
 			echo "CentOs 7 detectado" | tee -a reporte.txt
-			dependencias_rh
+			#pm es una variable de entorno
+			pm=yum
+			dependencias
 		else
 			#Si es Debian 8
-			uname -r | grep 3.16.0-6-amd64
+			uname -r | grep "3.16.0-\(.*\)-amd64"
 			if [ $? -eq 0 ]; then
 				echo "Debian 8 detectado" | tee -a reporte.txt
-				dependencias_dev
+				pm=apt
+				dependencias
 			#Si no se detecto ninguno, no se instala nada
 			#Termina la ejecucion con error
 			else
@@ -49,4 +50,4 @@ function main (){
 	fi
 }
 
-main
+revisa_distro
